d684b2b0e713752d51a628c46434f9e4e49f3600
Vathana <84817347+Vathana-Git@users.noreply.github.com>
2023-10-21 02:57:06 +0700

    Finish exercise 05 to 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  
+  if (typeof start != "number" || typeof end != "number") {
+    return "ERROR";
+  } else if (start < 0 || end < 0) {
+    return "ERROR";
+  }
+
+  let result = 0;
+
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+
+  // [start, end] = [end, start];
+
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+
+  return result;
 
 };
 
