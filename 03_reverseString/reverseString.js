c715bd87ba64e84d6159082a69c54c70006a4552
Vathana <84817347+Vathana-Git@users.noreply.github.com>
2023-10-20 21:25:32 +0700

    Finish exercise 02 to 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  
+  // return text.split("").reverse().join("");
+
+  let reversedText = "";
+
+  for (let i = text.length -1; i >= 0; i--) {
+    reversedText += text[i];
+  }
+
+  return reversedText;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...args) {
+  // for (let i = 0; i <= args.length; i++) {
+  //   let index = mainArray.indexOf(args[i]);
+  //   if (index > -1) {
+  //     mainArray.splice(index, 1);
+  //   }
+  // }
+  // return mainArray;
+
+  let newArray = [];
+
+  mainArray.forEach((item) => {
+
+    if(!args.includes(item)) {
+      newArray.push(item);
+    }
+
+  });
+
+  return newArray;
 
 };
 
